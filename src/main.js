0c55e614de74ca8550bae90bd119bcdc8af68775
NataliaMazurak <mazurak25@gmail.com>
2023-08-18 19:53:32 +0300

    modal-recipe

--- src/main.js
+++ src/main.js
@@ -7,3 +7,22 @@ import { filtersList, onFilterItemClick } from './js/search-filter';
 filtersList?.addEventListener('click', onFilterItemClick);
 
 import './js/slider-events.js'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import {openModalButton, modalRecipe, modal, closeModalButton, closeEsc, modalBackDrop, closeModal, openModal } from './js/modal-recipe';
