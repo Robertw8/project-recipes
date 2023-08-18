a73ad33647aa54772097b0119ac55c9afbba4148
liussi <125599487+liussi@users.noreply.github.com>
2023-08-18 15:49:03 +0300

    fix main

--- src/js/popular-recipes.js
+++ src/js/popular-recipes.js
@@ -41,4 +41,3 @@ export async function renderElement() {
     }
 }
    
-renderElement()
--- src/main.js
+++ src/main.js
@@ -1,18 +1,18 @@
 import { testFn } from './js/test';
+
 testFn();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 import { getImageApi, createMarkup, renderElement } from './js/popular-recipes.js';
+renderElement();
+
+
+
+
+
+
+
+
+
+
+
+
+
