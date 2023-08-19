25a31ec5783f02a6ba43334d6ed8cf0d0eda5ee9
Roman Kryhul <104628639+UmyaFamilia@users.noreply.github.com>
2023-08-19 21:34:39 +0300

    try agein

--- src/main.js
+++ src/main.js
@@ -1,60 +1,60 @@
-// import { getRequestsService } from './js/API/api-service.js';
-// import debounce from 'lodash.debounce';
+import { getRequestsService } from './js/API/api-service.js';
+import debounce from 'lodash.debounce';
 
-// import {
-//   getImageApi,
-//   createMarkup,
-//   renderElement,
-// } from './js/popular-recipes.js';
-// renderElement();
+import {
+  getImageApi,
+  createMarkup,
+  renderElement,
+} from './js/popular-recipes.js';
+renderElement();
 
-// import { setLocalStorageTheme, setThemeOnClick } from './js/dark-theme.js';
+import { setLocalStorageTheme, setThemeOnClick } from './js/dark-theme.js';
 
-// import './js/slider-events.js';
+import './js/slider-events.js';
 
-// import { renderRecipes } from './js/recipes-render';
+import { renderRecipes } from './js/recipes-render';
 
-// import {
-//   openModalButton,
-//   modalRecipe,
-//   modal,
-//   closeModalButton,
-//   closeEsc,
-//   modalBackDrop,
-//   closeModal,
-//   openModal,
-// } from './js/modal-recipe';
+import {
+  openModalButton,
+  modalRecipe,
+  modal,
+  closeModalButton,
+  closeEsc,
+  modalBackDrop,
+  closeModal,
+  openModal,
+} from './js/modal-recipe';
 
-// import {
-//   extraFilterRefs,
-//   onFilterItemClick,
-//   renderAreaOptions,
-//   renderIngredientsOptions,
-//   onSearchInput,
-//   onResetBtnClick,
-//   filterByArea,
-//   filterByIngredient,
-//   filterByTime,
-// } from './js/search-filter';
+import {
+  extraFilterRefs,
+  onFilterItemClick,
+  renderAreaOptions,
+  renderIngredientsOptions,
+  onSearchInput,
+  onResetBtnClick,
+  filterByArea,
+  filterByIngredient,
+  filterByTime,
+} from './js/search-filter';
 
-// getRequestsService('recipes').then(data => {
-//   renderAreaOptions(data);
-// });
+getRequestsService('recipes').then(data => {
+  renderAreaOptions(data);
+});
 
-// getRequestsService('ingredients').then(data => {
-//   renderIngredientsOptions(data);
-// });
+getRequestsService('ingredients').then(data => {
+  renderIngredientsOptions(data);
+});
 
-// extraFilterRefs.filtersList?.addEventListener('click', onFilterItemClick);
-// extraFilterRefs.input?.addEventListener('input', debounce(onSearchInput, 300));
-// extraFilterRefs.resetBtn?.addEventListener('click', onResetBtnClick);
-// extraFilterRefs.areaFilter?.addEventListener('click', filterByArea);
-// extraFilterRefs.ingredientsFilter?.addEventListener(
-//   'click',
-//   filterByIngredient
-// );
-// extraFilterRefs.timeFilter?.addEventListener('click', filterByTime);
+extraFilterRefs.filtersList?.addEventListener('click', onFilterItemClick);
+extraFilterRefs.input?.addEventListener('input', debounce(onSearchInput, 300));
+extraFilterRefs.resetBtn?.addEventListener('click', onResetBtnClick);
+extraFilterRefs.areaFilter?.addEventListener('click', filterByArea);
+extraFilterRefs.ingredientsFilter?.addEventListener(
+  'click',
+  filterByIngredient
+);
+extraFilterRefs.timeFilter?.addEventListener('click', filterByTime);
 
-// import './js/slider-events.js';
+import './js/slider-events.js';
 import { favorites } from './js/favorites.js';
 favorites();
