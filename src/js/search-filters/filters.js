17401b5550e48d134d8b77ca0ed208cf25507899
Robertw8 <oniedgaf@gmail.com>
2023-08-20 03:03:26 +0300

    refactored search filters js

--- src/js/search-filters/filters.js
+++ src/js/search-filters/filters.js
@@ -6,23 +6,26 @@ const { selectedArea, selectedIngredient, selectedTime } = getFilterRefs();
 
 const filterByArea = e => {
   queryParams.areaQuery = e.target.dataset.area;
-
-  selectedArea.textContent = e.target.textContent;
+  updateSelectedContent(selectedArea, e.target.textContent);
   executeRequest();
 };
 
 const filterByIngredient = e => {
   queryParams.ingredientQuery = e.target.dataset.ingredient;
-
-  selectedIngredient.textContent = e.target.textContent;
+  updateSelectedContent(selectedIngredient, e.target.textContent);
   executeRequest();
 };
 
 const filterByTime = e => {
   queryParams.timeQuery = e.target.dataset.time;
-
-  selectedTime.textContent = e.target.textContent;
+  updateSelectedContent(selectedTime, e.target.textContent);
   executeRequest();
+};
+
+const updateSelectedContent = (element, content) => {
+  if (element instanceof Element) {
+    element.textContent = content;
+  }
 };
 
 export { filterByArea, filterByIngredient, filterByTime };
