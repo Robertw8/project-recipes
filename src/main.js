f16e7a21e472922de172cc5e3610f5d847570a18
NataliaMazurak <mazurak25@gmail.com>
2023-08-20 14:22:10 +0300

    fixt main

--- src/main.js
+++ src/main.js
@@ -18,49 +18,6 @@ import './js/modal-recipe';
 import './js/search-filters/handlers.js';
 import './js/search-filters/filters.js';
 
-// <<<<<<< HEAD
-const {
-  filtersList,
-  searchInput,
-  resetBtn,
-  areaFilter,
-  ingredientsFilter,
-  timeFilter,
-} = getFilterRefs();
-
-getRequestsService('recipes').then(data => {
-  renderAreaOptions(data);
-});
-
-getRequestsService('ingredients').then(data => {
-  renderIngredientsOptions(data);
-});
-
-// <<<<<<< HEAD
-extraFilterRefs.filtersList?.addEventListener('click', onFilterItemClick);
-extraFilterRefs.input?.addEventListener('input', debounce(onSearchInput, 300));
-extraFilterRefs.resetBtn?.addEventListener('click', onResetBtnClick);
-extraFilterRefs.areaFilter?.addEventListener('click', filterByArea);
-extraFilterRefs.ingredientsFilter?.addEventListener(
-  'click',
-  filterByIngredient
-);
-extraFilterRefs.timeFilter?.addEventListener('click', filterByTime);
-
-import './js/slider-events.js';
-
-
-// =======
-filtersList?.addEventListener('click', onFilterItemClick);
-searchInput?.addEventListener('input', debounce(onSearchInput, 300));
-resetBtn?.addEventListener('click', onResetBtnClick);
-areaFilter?.addEventListener('click', filterByArea);
-ingredientsFilter?.addEventListener('click', filterByIngredient);
-timeFilter?.addEventListener('click', filterByTime);
-//?----------------------------------------search filters-------------------------------------------
-// >>>>>>> main
-// =======
-//? Favorites
 import { favorites } from './js/favorites.js';
 favorites();
-// >>>>>>> main
+
