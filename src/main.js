3e4f719650576dfcf9c7f0f7a4c89e6b811ac9f0
Roman Kryhul <104628639+UmyaFamilia@users.noreply.github.com>
2023-08-20 20:19:29 +0300

    try again

--- src/main.js
+++ src/main.js
@@ -1,24 +1,24 @@
-//? Popular recipes
-import { renderElement } from './js/popular-recipes.js';
-renderElement();
+// //? Popular recipes
+// import { renderElement } from './js/popular-recipes.js';
+// renderElement();
 
-//? Theme switch
-import { setLocalStorageTheme, setThemeOnClick } from './js/dark-theme.js';
+// //? Theme switch
+// import { setLocalStorageTheme, setThemeOnClick } from './js/dark-theme.js';
 
-//? Slider
-import './js/slider-events.js';
+// //? Slider
+// import './js/slider-events.js';
 
-//? Recipes list
-import { renderRecipes } from './js/recipes-render';
+// //? Recipes list
+// import { renderRecipes } from './js/recipes-render';
 
-//? Recipes modal
-import './js/modal-recipe';
+// //? Recipes modal
+// import './js/modal-recipe';
 
-//? Search filters
-import './js/search-filters/handlers.js';
-import './js/search-filters/filters.js';
+// //? Search filters
+// import './js/search-filters/handlers.js';
+// import './js/search-filters/filters.js';
 
-//? Favorites
+// //? Favorites
 import { favorites } from './js/favorites.js';
 favorites();
 
