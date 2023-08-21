ea8db995e8270adb44bdd6234fe2c18a6e52151e
Robertw8 <oniedgaf@gmail.com>
2023-08-21 22:29:46 +0300

    fix order modal js

--- src/js/order-popup.js
+++ src/js/order-popup.js
@@ -1,103 +1,91 @@
-import { postOrder } from '../api';
+// import { postOrder } from '../api';
 import Swal from 'sweetalert2';
-import { disablePageScroll, enablePageScroll } from 'scroll-lock';
 
 const refs = {
-    modalOrderNowForm: document.querySelector('.order-now-modal-form'),
-    openButtonEl: document.querySelector('.header-order-btn'),
-    openButtonHeroEl: document.querySelector('.hero-btn'),
-    closeButtonEl: document.querySelector('.order-now-modal-close-btn'),
-    backdropEl: document.querySelector('.js-backdrop'),
+  modalOrderNowForm: document.querySelector('.order-now-modal-form'),
+  openButtonEl: document.querySelector('.header-order-btn'),
+  openButtonHeroEl: document.querySelector('.hero-btn'),
+  closeButtonEl: document.querySelector('.order-now-modal-close-btn'),
+  backdropEl: document.querySelector('.order-now-backdrop'),
 };
 
-if (refs.openButtonHeroEl) {
-    refs.openButtonHeroEl.addEventListener('click', onModalOpen, {
-        passive: true,
-    });
-}
-
-refs.openButtonEl.addEventListener('click', onModalOpen, { passive: true });
-refs.closeButtonEl.addEventListener('click', onModalRemove, { passive: true });
-refs.backdropEl.addEventListener('click', onBackdropClick, { passive: true });
-refs.modalOrderNowForm.addEventListener('submit', onSubmitForm, {
-    passive: true,
+refs.openButtonEl?.addEventListener('click', onModalOpen, { passive: true });
+refs.openButtonHeroEl?.addEventListener('click', onModalOpen);
+refs.closeButtonEl?.addEventListener('click', onModalRemove, { passive: true });
+refs.backdropEl?.addEventListener('click', onBackdropClick, { passive: true });
+refs.modalOrderNowForm?.addEventListener('submit', onSubmitForm, {
+  passive: true,
 });
 
 function onModalOpen() {
-    disablePageScroll();
-    window.addEventListener('keydown', onEscKeyPress, { passive: true });
-    document.body.classList.add('show-modal-order-now');
+  window.addEventListener('keydown', onEscKeyPress, { passive: true });
+  document.body.classList.add('show-modal-order-now');
 }
 
 function onModalRemove() {
-    window.removeEventListener('keydown', onEscKeyPress);
-    document.body.classList.remove('show-modal-order-now');
-    enablePageScroll();
+  window.removeEventListener('keydown', onEscKeyPress);
+  document.body.classList.remove('show-modal-order-now');
 }
 
 function onBackdropClick(event) {
-    if (event.currentTarget === event.target) {
-        onModalRemove();
-    }
+  if (event.currentTarget === refs.backdropEl) {
+    onModalRemove();
+  }
 }
 
 function onEscKeyPress(event) {
-    if (event.code === 'Escape') {
-        onModalRemove();
-    }
+  if (event.code === 'Escape') {
+    onModalRemove();
+  }
 }
 
 function extractFormData(form) {
-    let formData = {};
+  let formData = {};
 
-    formData.name = form.querySelector('input[name="name"]').value;
-    formData.phone = form.querySelector('input[name="phone"]').value;
-    formData.email = form.querySelector('input[name="email"]').value;
-    formData.comment = form.querySelector('textarea[name="comment"]').value;
-    if (form.querySelector('textarea[name="comment"]').value === '') {
-        formData.comment = 'So delicious';
-    }
-    return formData;
+  formData.name = form.querySelector('input[name="name"]').value;
+  formData.phone = form.querySelector('input[name="phone"]').value;
+  formData.email = form.querySelector('input[name="email"]').value;
+  formData.comment = form.querySelector('textarea[name="comment"]').value;
+  if (form.querySelector('textarea[name="comment"]').value === '') {
+    formData.comment = 'So delicious';
+  }
+  return formData;
 }
 
 function onSubmitForm(e) {
-    e.preventDefault();
+  e.preventDefault();
 
-    const formData = extractFormData(refs.modalOrderNowForm);
+  const formData = extractFormData(refs.modalOrderNowForm);
 
-    const { name, phone, email, comment } = formData;
+  // const { name, phone, email, comment } = formData;
 
-    const options = {
-        name,
-        phone,
-        email,
-        comment,
-    };
+  // const options = {
+  //   name,
+  //   phone,
+  //   email,
+  //   comment,
+  // };
 
-    postOrder(options)
-        .then(() => {
-            if (localStorage.getItem('patch-rating') !== 'error') {
-                onModalRemove();
-                setTimeout(() => {
-                    Swal.fire({
-                        icon: 'success',
-                        title: 'Thank you for your order!',
-                        timer: 1500,
-                        showConfirmButton: false
-                    });
-                }, 500);
-                refs.modalOrderNowForm.reset();
-            }
-            return;
-        })
-        .catch(error => console.log(error))
-        .finally(
-            setTimeout(() => {
-                Loading.remove();
-            }, 500)
-        );
-
-
+  // postOrder(options)
+  //   .then(() => {
+  //     if (localStorage.getItem('patch-rating') !== 'error') {
+  //       onModalRemove();
+  //       setTimeout(() => {
+  //         Swal.fire({
+  //           icon: 'success',
+  //           title: 'Thank you for your order!',
+  //           timer: 1500,
+  //           showConfirmButton: false,
+  //         });
+  //       }, 500);
+  //       refs.modalOrderNowForm.reset();
+  //     }
+  //     return;
+  //   })
+  //   .catch(error => console.log(error))
+  //   .finally(
+  //     setTimeout(() => {
+  //       Loading.remove();
+  //     }, 500)
+  //   );
 }
-
-export { onModalOpen };
